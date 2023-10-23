18079c7c501c096c2b3d1a13c5061a435fdaed06
nvdinh185 <nvdinh0766777123@gmail.com>
2023-10-23 14:42:53 +0700

    x

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = 3001;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname + '/client/build'));
 
 const route = require('./routes');
 
